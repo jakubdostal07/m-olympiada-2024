4c60d2a614d2ed3a27591c0b6f902aaa3524a7f0
Jakub Dostál <jakub.dostal07@gmail.com>
2024-10-17 20:42:31 +0200

    Už to funguje správně ale pouze na mojem PC :(

--- DomaciKolo/P-I-2/src/main.cpp
+++ DomaciKolo/P-I-2/src/main.cpp
@@ -1,169 +1,177 @@
 #include <iostream>
 #include <vector>
 #include <stdexcept>
-#include <format>
+// #include <format>
 #include <map>
+#include <algorithm>
 
-struct item_entry {
+struct ItemEntry {
 	bool nabidka;
 	int id;
 	int price;
 	int poradi;
 };
 
-int countDifferentIds(std::vector<item_entry> entry_vec) {
-	std::map<int, int> different_ids;
+class Transakce {
+	public:
+	ItemEntry nabidka;
+	ItemEntry poptavka;
 
-	for (int i = 0; i < (int)entry_vec.size(); i++) {
-		different_ids[entry_vec[i].id] += 1;
-	}
-
-	return (int)different_ids.size();
-}
-
-int getLowestItemId(std::vector<item_entry> entry_vec, int max_id) {
-	int lowest = max_id;
-
-	for (int i = 0; i < (int)entry_vec.size(); i++) {
-		if (entry_vec[i].id < lowest) {
-			lowest = entry_vec[i].id;
+	Transakce(ItemEntry nabidka, ItemEntry poptavka) {
+		if (not nabidka.nabidka) {
+			throw std::logic_error("nabidka neni nabidka");
 		}
+
+		if (poptavka.nabidka) {
+			throw std::logic_error("poptavka neni poptavka");
+		}
+
+		this->nabidka = nabidka;
+		this->poptavka = poptavka;
 	}
 
-	return lowest;
+	int getProfit() {
+		return this->poptavka.price - this->nabidka.price;
+	}
+};
+
+int compareTransactionProfit(Transakce a, Transakce b) {
+	return a.getProfit() > b.getProfit();
 }
 
-int getProfit(std::pair<item_entry, item_entry> transaction) {
-	item_entry nabidka = transaction.first;
-	item_entry poptavka = transaction.second;
+int getProfit(std::pair<ItemEntry, ItemEntry> transaction) {
+	ItemEntry nabidka = transaction.first;
+	ItemEntry poptavka = transaction.second;
 	return poptavka.price - nabidka.price;
 }
 
 int main(int argc, char *argv[]) {
 	int entry_count;
-	std::vector<item_entry> entry_vec;
+	std::vector<ItemEntry> nabidky;
+	std::vector<ItemEntry> poptavky;
 
 	// Read input
 	std::cin >> entry_count;
 
 	for (int i = 0; i < entry_count; i++) {
-		item_entry vstup;
+		ItemEntry item;
 		
 		char znak;
-		std::cin >> znak >> vstup.id >> vstup.price;
+		std::cin >> znak >> item.id >> item.price;
 
 		if (znak == 'N') {
-			vstup.nabidka = true;
+			item.nabidka = true;
 		} else if (znak == 'P') {
-			vstup.nabidka = false;
+			item.nabidka = false;
 		} else {
-			throw std::logic_error(std::format("Znak vstupu neni 'N' nebo 'P' ale {}", znak));
+			throw std::logic_error("Znak vstupu neni 'N' nebo 'P'");
 		}
 
-		vstup.poradi = i;
+		item.poradi = i;
 
-		entry_vec.push_back(vstup);
+		if (item.nabidka) {
+			nabidky.push_back(item);
+		} else {
+			poptavky.push_back(item);
+		}
 	}
 
-	const int MIN_ID = getLowestItemId(entry_vec, entry_count);
-	const int MAX_ID = entry_count;
+	std::vector<Transakce> mozne_transakce;
+	
+	// Pro kazdou nabidku najdeme poptavku a pokusime se vytvorit transakci
+	for (int index_nabidky = 0; index_nabidky < (int)nabidky.size(); index_nabidky++) {
+		ItemEntry nabidka = nabidky[index_nabidky];
 
-	std::cout << "MIN_ID = " << MIN_ID << std::endl;
-	std::cout << "MAX_ID = " << MAX_ID << std::endl; 
+		for (int index_poptavky = 0; index_poptavky < (int)poptavky.size(); index_poptavky++) {
+			ItemEntry poptavka = poptavky[index_poptavky];
 
-	// Najdeme nejvyhodnejsi nabidku a poptavku pro kazdou entry
-	std::map<int, item_entry> nejvyhodnejsi_nabidky;
-	std::map<int, item_entry> nejvyhodnejsi_poptavky;
-	std::vector<item_entry> nabidky;
-	std::vector<item_entry> poptavky;
-
-	// Pro kazdy item najdeme nejvyhodnejsi nabidku a poptavku
-	for (int item_id = MIN_ID; item_id <= MAX_ID; item_id++) {
-		for (int i = 0; i < (int)entry_vec.size(); i++) {
-			item_entry item = entry_vec[i];
-
-			if (item_id != item.id) {
+			// Pokud nemáme stejné předměty v nabídce a poptávce nebo se nabídka objevila později než poptávka
+			// Tak přeskočíme pár
+			if ((nabidka.id != poptavka.id) || (nabidka.poradi > poptavka.poradi)) {
 				continue;
 			}
 
-			if (item.nabidka) {
-				// Nabidka
-				nabidky.push_back(item);
+			Transakce mozna_transakce = Transakce(nabidka, poptavka);
+			mozne_transakce.push_back(mozna_transakce);
+		}
+	}
 
-				if (nejvyhodnejsi_nabidky[item_id].id == 0) {
-					nejvyhodnejsi_nabidky[item_id] = item;
-				} else if (item.price < nejvyhodnejsi_nabidky[item_id].price) {
-					nejvyhodnejsi_nabidky[item_id] = item;
-				}
+	// std::cout << "Nabídky:" << std::endl;
+	// for (int i = 0; i < (int)nabidky.size(); i++) {
+	// 	std::cout << std::format("id {}; price {}; pořadí {}", nabidky[i].id, nabidky[i].price, nabidky[i].poradi) << "\n";
+	// }
 
-			} else {
-				// Poptavka
-				poptavky.push_back(item);
+	// std::cout << "Poptávky:" << std::endl;
+	// for (int i = 0; i < (int)poptavky.size(); i++) {
+	// 	std::cout << std::format("id {}; price {}; pořadí {}", poptavky[i].id, poptavky[i].price, poptavky[i].poradi) << "\n";
+	// }
 
-				if (nejvyhodnejsi_poptavky[item_id].id == 0) {
-					nejvyhodnejsi_poptavky[item_id] = item;
-				} else if (item.price > nejvyhodnejsi_poptavky[item_id].price) {
-					nejvyhodnejsi_poptavky[item_id] = item;
-				}
+	// std::cout << "Možné transakce:" << std::endl;
+	// for(int i = 0; i < (int)mozne_transakce.size(); i++) {
+	// 	Transakce transakce = mozne_transakce[i];
+	// 	std::cout << std::format("nabídka {}; poptávka {}; profit {}", transakce.nabidka.poradi, transakce.poptavka.poradi, transakce.getProfit()) << "\n";
+	// }
+
+	// Seřadíme možné transakce podle výše profitu
+	std::sort(mozne_transakce.begin(), mozne_transakce.end(), compareTransactionProfit);
+
+	// std::cout << "Seřazené možné transakce:" << std::endl;
+	// for(int i = 0; i < (int)mozne_transakce.size(); i++) {
+	// 	Transakce transakce = mozne_transakce[i];
+	// 	std::cout << std::format("nabídka {}; poptávka {}; profit {}", transakce.nabidka.poradi, transakce.poptavka.poradi, transakce.getProfit()) << "\n";
+	// }
+
+	// Pokusíme se maximalizovat profit pomocí provádění co nejvíce transakcí co můžeme
+	std::vector<Transakce> vybrane_transakce;
+
+	if ((int)mozne_transakce.size() > 0) {
+		vybrane_transakce.push_back(mozne_transakce[0]); // Přidáme nejvýhodnější transakci
+	} else {
+		std::cout << 0 << std::endl;
+		return 0;
+	}
+
+	// Zkontrolujeme zda lze provést další transakce
+	// Začínáme na indexu 1 protože 0 už byl přidán
+	for (int index_mozne_transakce = 1; index_mozne_transakce < (int)mozne_transakce.size(); index_mozne_transakce++) {
+		Transakce mozna_transakce = mozne_transakce[index_mozne_transakce];
+		bool valid_transakce = false;
+
+		for (int index_vtransakce = 0; index_vtransakce < (int)vybrane_transakce.size(); index_vtransakce++) {
+			Transakce vybrana_transakce = vybrane_transakce[index_vtransakce];
+
+			// Využijeme jednoduchý algorytmus na detekci kolize dvou 1D čar
+			int x1 = mozna_transakce.nabidka.poradi;
+			int x2 = mozna_transakce.poptavka.poradi;
+			int y1 = vybrana_transakce.nabidka.poradi;
+			int y2 = vybrana_transakce.poptavka.poradi;
+
+			if ((y1 < x1 && y1 < x2 && y2 < x1 && y2 < x2) || (y1 > x1 && y1 > x2 && y2 > x1 && y2 > x2)) {
+				valid_transakce = true;
+				break;
 			}
 		}
-	}
 
-	std::cout << "Nabidky:" << std::endl;
-	for (int i = 0; i < (int)nabidky.size(); i++) {
-		std::cout << std::format("id {}; price {}; poradi {}", nabidky[i].id, nabidky[i].price, nabidky[i].poradi) << "\n";
-	}
-
-	std::cout << "Poptavky:" << std::endl;
-	for (int i = 0; i < (int)poptavky.size(); i++) {
-		std::cout << std::format("id {}; price {}; poradi {}", poptavky[i].id, poptavky[i].price, poptavky[i].poradi) << "\n";
-	}
-
-	std::cout << "Nejvyhodnejsi nabidky:" << std::endl;
-	for(const auto& elem : nejvyhodnejsi_nabidky) {
-		std::cout << std::format("id {}; price {}; poradi {}", elem.second.id, elem.second.price, elem.second.poradi) << "\n";
-	}
-
-	std::cout << "Nejvyhodnejsi poptavky:" << std::endl;
-	for(const auto& elem : nejvyhodnejsi_poptavky) {
-		std::cout << std::format("id {}; price {}; poradi {}", elem.second.id, elem.second.price, elem.second.poradi) << "\n";
-	}
-
-	std::vector<std::pair<item_entry, item_entry>> mozne_transakce;
-
-	for (int item_id = MIN_ID; item_id <= MAX_ID; item_id++) {
-		
-		if (nejvyhodnejsi_nabidky[item_id].id != 0 and nejvyhodnejsi_poptavky[item_id].id != 0 and // Mame par pro transakci??
-			nejvyhodnejsi_nabidky[item_id].poradi < nejvyhodnejsi_poptavky[item_id].poradi) { // Byla nabidka polozena drive nez poptavka
-			item_entry nabidka = nejvyhodnejsi_nabidky[item_id];
-			item_entry poptavka = nejvyhodnejsi_poptavky[item_id];
-			mozne_transakce.push_back(std::pair<item_entry, item_entry>(nabidka, poptavka));
+		if (valid_transakce == true) {
+			vybrane_transakce.push_back(mozna_transakce);
 		}
 	}
 
-	std::pair<item_entry, item_entry> nejvyhodnejsi_transakce;
-	for (int i = 0; i < (int)mozne_transakce.size(); i++) {
-		std::pair<item_entry, item_entry> transakce = mozne_transakce[i];
-		item_entry nabidka = transakce.first;
-		item_entry poptavka = transakce.second;
-		int zisk = poptavka.price - nabidka.price;
-
-		std::cout << std::format("--- Nabidka {} ---", i) << std::endl;
-		std::cout << std::format("Nabidka id {}; cena {}; poradi {}", nabidka.id, nabidka.price, nabidka.poradi) << std::endl;
-		std::cout << std::format("Poptavka id {}; cena {}; poradi {}", poptavka.id, poptavka.price, poptavka.poradi) << std::endl;
-		std::cout << std::format("Mozny zisk {}", zisk) << std::endl;
-
-		if (zisk > getProfit(nejvyhodnejsi_transakce)) {
-			nejvyhodnejsi_transakce = transakce;
-		}
+	// std::cout << "Vybrané transakce:" << std::endl;
+	int total_profit = 0;
+	for(int i = 0; i < (int)vybrane_transakce.size(); i++) {
+		Transakce transakce = vybrane_transakce[i];
+		// std::cout << std::format("nabídka {}; poptávka {}; profit {}", transakce.nabidka.poradi, transakce.poptavka.poradi, transakce.getProfit()) << "\n";
+		total_profit += transakce.getProfit();
 	}
 
-	std::cout << "--- Nejvyhodnejsi transakce ---" << std::endl;
-	std::cout << std::format("Nabidka: id {}; cena {}; poradi {}", nejvyhodnejsi_transakce.first.id,
-										nejvyhodnejsi_transakce.first.price, nejvyhodnejsi_transakce.first.poradi) << std::endl;
-	std::cout << std::format("Poptavka: id {}; cena {}; poradi {}", nejvyhodnejsi_transakce.second.id,
-										nejvyhodnejsi_transakce.second.price, nejvyhodnejsi_transakce.second.poradi) << std::endl;
-	std::cout << std::format("Zisk transakce {}", getProfit(nejvyhodnejsi_transakce)) << std::endl;
+	// std::cout << "Finální profit: " << total_profit << std::endl;
+	
+	if (total_profit < 0) {
+		total_profit = 0;
+	}
+
+	std::cout << total_profit << std::endl;
 
 	return 0;
 }
